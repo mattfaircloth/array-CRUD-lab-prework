debfffcb0aeaea0d7ee73108fb03018463b2e2f8
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-23 23:34:44 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  soccer_teams << "Burnley"
 end
 
 def add_element_to_start_of_array(array, element)
-
+  soccer_teams.unshift("Tottenham")
 end
 
 def remove_element_from_end_of_array(array)
